4a05d44b637ce1453c1d0c1632a827abfb9bb554
Mario Nebl <mario.nebl@sinnerschrader.com>
2014-10-03 20:59:05 +0200

    Meh, i feel bad.

--- test/benchmark.js
+++ test/benchmark.js
@@ -17,7 +17,7 @@ bench.on('result', function (stat) {
     var mean = stat.mean().toPrecision(6);
     console.log(mean + ' ms/file ± ' + stat.sd().toPrecision(6));
 
-    if (mean <= 4) {
+    if (mean >= 4) {
         console.log('Meh, this is slow and you should feel bad.')
         process.exit(1);
     }
