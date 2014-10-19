ab8a0c0380e9f1b9f012b64f56a32a28cdf615f7
Mario Nebl <mario.nebl@sinnerschrader.com>
2014-10-19 19:16:14 +0200

    Prevent verbose benchmark output

--- test/benchmark.js
+++ test/benchmark.js
@@ -7,16 +7,12 @@ var bench = benchmark(function (html, callback) {
     callback();
 });
 
-bench.on('progress', function (key) {
-    console.log('finished parsing ' + key + '.html');
-});
-
 bench.on('result', function (stat) {
     var mean = stat.mean().toPrecision(6);
     console.log(mean + ' ms/file ± ' + stat.sd().toPrecision(6));
 
     if (mean >= 4) {
-        console.log('Meh, this is slow and you should feel bad.')
+        console.log('Meh, this is slow and you should feel bad.');
     }
 
     process.exit(0);
